d99b5d38692c61fc82a1c8c1dd7e1c88c18fc8a0
mohammed-mahmmud20 <111295776+mohammed-mahmmud20@users.noreply.github.com>
2022-09-21 02:58:23 +0200

    Update About.jsx

--- src/component/About/About.jsx
+++ src/component/About/About.jsx
@@ -4,176 +4,175 @@ import "./About.css"
 const About = () => {
     return (
         <div>
-            <div className="container">
-            <div class="container">
+//             <div className="container">
+//             <div class="container">
 
-            <div class="section-title">
-                <h2>Resume</h2>
-                <p>Check My Resume</p>
-            </div>
+//             <div class="section-title">
+//                 <h2>Resume</h2>
+//                 <p>Check My Resume</p>
+//             </div>
 
-            <div class="row">
-                <div class="col-lg-6">
-                <h3 class="resume-title">summary</h3>
-                <div class="resume-item pb-0">
-                    <h4>Abdulrahman Alaa</h4>
-                    <p><em>Full Stack Developer with 5+ Years of experience of website development .</em></p>
-                    <p>
-                    <ul>
-                    <li>Octobar, Giza, Egypt</li>
-                    <li>+201113107599</li>
-                    <li>abdulrahmaanalaa1999@gmail.com</li>
-                    </ul>
-                    </p>
-                </div>
+//             <div class="row">
+//                 <div class="col-lg-6">
+//                 <h3 class="resume-title">summary</h3>
+//                 <div class="resume-item pb-0">
+//                     <h4>Abdulrahman Alaa</h4>
+//                     <p><em>Full Stack Developer with 5+ Years of experience of website development .</em></p>
+//                     <p>
+//                     <ul>
+//                     <li>Octobar, Giza, Egypt</li>
+//                     <li>+201113107599</li>
+//                     <li>abdulrahmaanalaa1999@gmail.com</li>
+//                     </ul>
+//                     </p>
+//                 </div>
 
-                <h3 class="resume-title">Skills</h3>
-                <div class="resume-item">
-                    <h4>Full Stack Developer &amp; Instructor</h4>
-                    <h5>My Skills :</h5>
-                    <p><em>HTML & HTML5 - CSS & CSS3 - BootStrap - JavaScript - ReactJs - Redux - Hooks - PHP - mySQL - Laravel - Git & GitHub - Sass - PugJs </em></p>
-                </div>
-                </div>
-
-
+//                 <h3 class="resume-title">Skills</h3>
+//                 <div class="resume-item">
+//                     <h4>Full Stack Developer &amp; Instructor</h4>
+//                     <h5>My Skills :</h5>
+//                     <p><em>HTML & HTML5 - CSS & CSS3 - BootStrap - JavaScript - ReactJs - Redux - Hooks - PHP - mySQL - Laravel - Git & GitHub - Sass - PugJs </em></p>
+//                 </div>
+//                 </div>
 
 
-                <div class="col-lg-6">
-                <h3 class="resume-title">Professional Experience</h3>
-                <div class="resume-item">
-                    <h4>Instructor Full Stack Developer</h4>
-                    <h5>2019 - Present</h5>
-                    <p><em>X-Team Company </em></p>
-                    <p>
-                    <ul>
-                    <li>Position: Team Leader </li>
-                    <li>Salary: 20,000EGP</li>
-                    <li>Language: JavaScript, PHP</li>
-                    <li>Database: mySQL</li>
-                    <li>Tec: ReactJs, BootStrap, Laravel</li>
-                    </ul>
-                    </p>
-                </div>
-                <div class="resume-item">
-                    <h4>Instructor Full Stack Developer</h4>
-                    <h5>Present</h5>
-                    <p><em>Create My Own Courses Online </em></p>
-                    <p>
-                    <ul>
-                    <li>Programming Content Creator</li>
-                    <li>Social Media like, Instagram, Tiktok, Facebook</li>
-                    <li>Create Videos on my Youtube Channel</li>
-                    </ul>
-                    </p>
-                </div>
-                <div class="resume-item">
-                    <h4>Instructor Front end Developer</h4>
-                    <h5>2017 - 2019</h5>
-                    <p><em>100 thousand programmers</em></p>
-                    <p>
-                    <ul>
-                    <li>I helped many students in this field of programming.</li>
+
+
+//                 <div class="col-lg-6">
+//                 <h3 class="resume-title">Professional Experience</h3>
+//                 <div class="resume-item">
+//                     <h4>Instructor Full Stack Developer</h4>
+//                     <h5>2019 - Present</h5>
+//                     <p><em>X-Team Company </em></p>
+//                     <p>
+//                     <ul>
+//                     <li>Position: Team Leader </li>
+//                     <li>Salary: 20,000EGP</li>
+//                     <li>Language: JavaScript, PHP</li>
+//                     <li>Database: mySQL</li>
+//                     <li>Tec: ReactJs, BootStrap, Laravel</li>
+//                     </ul>
+//                     </p>
+//                 </div>
+//                 <div class="resume-item">
+//                     <h4>Instructor Full Stack Developer</h4>
+//                     <h5>Present</h5>
+//                     <p><em>Create My Own Courses Online </em></p>
+//                     <p>
+//                     <ul>
+//                     <li>Programming Content Creator</li>
+//                     <li>Social Media like, Instagram, Tiktok, Facebook</li>
+//                     <li>Create Videos on my Youtube Channel</li>
+//                     </ul>
+//                     </p>
+//                 </div>
+//                 <div class="resume-item">
+//                     <h4>Instructor Front end Developer</h4>
+//                     <h5>2017 - 2019</h5>
+//                     <p><em>100 thousand programmers</em></p>
+//                     <p>
+//                     <ul>
+//                     <li>I helped many students in this field of programming.</li>
                 
-                    </ul>
-                    </p>
-                </div>
-                </div>
-            </div>
+//                     </ul>
+//                     </p>
+//                 </div>
+//                 </div>
+//             </div>
 
-            </div>
-            </div>
-            {/* ======= Resume Section =======  */}
-            {/* <section id="resume" class="resume">
-                <div class="container">
+//             </div>
+//             </div>
+//             {/* ======= Resume Section =======  */}
+//             {/* <section id="resume" class="resume">
+//                 <div class="container">
 
-                <div class="section-title">
-                    <h2>Resume</h2>
-                    <p>Check My Resume</p>
-                </div>
+//                 <div class="section-title">
+//                     <h2>Resume</h2>
+//                     <p>Check My Resume</p>
+//                 </div>
 
-                <div class="row">
-                    <div class="col-lg-6">
-                    <h3 class="resume-title">summary</h3>
-                    <div class="resume-item pb-0">
-                        <h4>Abdulrahman Alaa</h4>
-                        <p><em>Full Stack Developer with 5+ Years of experience of website development .</em></p>
-                        <p>
-                        <ul>
-                        <li>Octobar, Giza, Egypt</li>
-                        <li>+201113107599</li>
-                        <li>abdulrahmaanalaa1999@gmail.com</li>
-                        </ul>
-                        </p>
-                    </div>
+//                 <div class="row">
+//                     <div class="col-lg-6">
+//                     <h3 class="resume-title">summary</h3>
+//                     <div class="resume-item pb-0">
+//                         <h4>Abdulrahman Alaa</h4>
+//                         <p><em>Full Stack Developer with 5+ Years of experience of website development .</em></p>
+//                         <p>
+//                         <ul>
+//                         <li>Octobar, Giza, Egypt</li>
+//                         <li>+201113107599</li>
+//                         <li>abdulrahmaanalaa1999@gmail.com</li>
+//                         </ul>
+//                         </p>
+//                     </div>
 
-                    <h3 class="resume-title">Skills</h3>
-                    <div class="resume-item">
-                        <h4>Full Stack Developer &amp; Instructor</h4>
-                        <h5>My Skills :</h5>
-                        <p><em>HTML & HTML5 - CSS & CSS3 - BootStrap - JavaScript - ReactJs - Redux - Hooks - PHP - mySQL - Laravel - Git & GitHub - Sass - PugJs </em></p>
-                    </div>
-                    <div class="resume-item">
+//                     <h3 class="resume-title">Skills</h3>
+//                     <div class="resume-item">
+//                         <h4>Full Stack Developer &amp; Instructor</h4>
+//                         <h5>My Skills :</h5>
+//                         <p><em>HTML & HTML5 - CSS & CSS3 - BootStrap - JavaScript - ReactJs - Redux - Hooks - PHP - mySQL - Laravel - Git & GitHub - Sass - PugJs </em></p>
+//                     </div>
+//                     <div class="resume-item">
                         
-                        <h4>Some Videos</h4>
-                        <h5><a href="https://www.tiktok.com/@front_enddeveloper_alaa/video/7034388315801406725?lang=en&is_copy_url=1&is_from_webapp=v1">a day in the life of a web dev</a></h5>
-                        <h5><a href="https://www.tiktok.com/@front_enddeveloper_alaa/video/7035700595130486021?lang=en&is_copy_url=1&is_from_webapp=v1">salary of programmers</a></h5>
-                        <h5><a href="https://www.tiktok.com/@front_enddeveloper_alaa/video/7031266976551030021?lang=en&is_copy_url=1&is_from_webapp=v1">Course 1-1-2022</a></h5>
-                        <h5><a href="https://www.tiktok.com/@front_enddeveloper_alaa/video/7026117231775894789?lang=en&is_copy_url=1&is_from_webapp=v1">How to learn Web Developer</a></h5>
-                        <h5><a href="https://www.tiktok.com/@front_enddeveloper_alaa/video/7021994706242489606?lang=en&is_copy_url=1&is_from_webapp=v1">Learn HTML</a></h5>
-                        <h5><a href="https://www.tiktok.com/@front_enddeveloper_alaa/video/7037575165114354950?lang=en&is_copy_url=1&is_from_webapp=v1">Web Development</a></h5>
-                        <h5><a href="https://www.tiktok.com/@front_enddeveloper_alaa/video/7037217150536289541?lang=en&is_copy_url=1&is_from_webapp=v1">Choose a junior dev in company</a></h5>
+//                         <h4>Some Videos</h4>
+//                         <h5><a href="https://www.tiktok.com/@front_enddeveloper_alaa/video/7034388315801406725?lang=en&is_copy_url=1&is_from_webapp=v1">a day in the life of a web dev</a></h5>
+//                         <h5><a href="https://www.tiktok.com/@front_enddeveloper_alaa/video/7035700595130486021?lang=en&is_copy_url=1&is_from_webapp=v1">salary of programmers</a></h5>
+//                         <h5><a href="https://www.tiktok.com/@front_enddeveloper_alaa/video/7031266976551030021?lang=en&is_copy_url=1&is_from_webapp=v1">Course 1-1-2022</a></h5>
+//                         <h5><a href="https://www.tiktok.com/@front_enddeveloper_alaa/video/7026117231775894789?lang=en&is_copy_url=1&is_from_webapp=v1">How to learn Web Developer</a></h5>
+//                         <h5><a href="https://www.tiktok.com/@front_enddeveloper_alaa/video/7021994706242489606?lang=en&is_copy_url=1&is_from_webapp=v1">Learn HTML</a></h5>
+//                         <h5><a href="https://www.tiktok.com/@front_enddeveloper_alaa/video/7037575165114354950?lang=en&is_copy_url=1&is_from_webapp=v1">Web Development</a></h5>
+//                         <h5><a href="https://www.tiktok.com/@front_enddeveloper_alaa/video/7037217150536289541?lang=en&is_copy_url=1&is_from_webapp=v1">Choose a junior dev in company</a></h5>
 
                         
-                    </div>
-                    </div>
+//                     </div>
+//                     </div>
 
 
 
 
-                    <div class="col-lg-6">
-                    <h3 class="resume-title">Professional Experience</h3>
-                    <div class="resume-item">
-                        <h4>Instructor Full Stack Developer</h4>
-                        <h5>2019 - Present</h5>
-                        <p><em>X-Team Company </em></p>
-                        <p>
-                        <ul>
-                        <li>Position: Team Leader </li>
-                        <li>Salary: 20,000EGP</li>
-                        <li>Language: JavaScript, PHP</li>
-                        <li>Database: mySQL</li>
-                        <li>Tec: ReactJs, BootStrap, Laravel</li>
-                        </ul>
-                        </p>
-                    </div>
-                    <div class="resume-item">
-                        <h4>Instructor Full Stack Developer</h4>
-                        <h5>Present</h5>
-                        <p><em>Create My Own Courses Online </em></p>
-                        <p>
-                        <ul>
-                        <li>Programming Content Creator</li>
-                        <li>Social Media like, Instagram, Tiktok, Facebook</li>
-                        <li>Create Videos on my Youtube Channel</li>
-                        </ul>
-                        </p>
-                    </div>
-                    <div class="resume-item">
-                        <h4>Instructor Front end Developer</h4>
-                        <h5>2017 - 2019</h5>
-                        <p><em>100 thousand programmers</em></p>
-                        <p>
-                        <ul>
-                        <li>I helped many students in this field of programming.</li>
+//                     <div class="col-lg-6">
+//                     <h3 class="resume-title">Professional Experience</h3>
+//                     <div class="resume-item">
+//                         <h4>Instructor Full Stack Developer</h4>
+//                         <h5>2019 - Present</h5>
+//                         <p><em>X-Team Company </em></p>
+//                         <p>
+//                         <ul>
+//                         <li>Position: Team Leader </li>
+//                         <li>Salary: 20,000EGP</li>
+//                         <li>Language: JavaScript, PHP</li>
+//                         <li>Database: mySQL</li>
+//                         <li>Tec: ReactJs, BootStrap, Laravel</li>
+//                         </ul>
+//                         </p>
+//                     </div>
+//                     <div class="resume-item">
+//                         <h4>Instructor Full Stack Developer</h4>
+//                         <h5>Present</h5>
+//                         <p><em>Create My Own Courses Online </em></p>
+//                         <p>
+//                         <ul>
+//                         <li>Programming Content Creator</li>
+//                         <li>Social Media like, Instagram, Tiktok, Facebook</li>
+//                         <li>Create Videos on my Youtube Channel</li>
+//                         </ul>
+//                         </p>
+//                     </div>
+//                     <div class="resume-item">
+//                         <h4>Instructor Front end Developer</h4>
+//                         <h5>2017 - 2019</h5>
+//                         <p><em>100 thousand programmers</em></p>
+//                         <p>
+//                         <ul>
+//                         <li>I helped many students in this field of programming.</li>
                     
-                        </ul>
-                        </p>
-                    </div>
-                    </div>
-                </div>
+//                         </ul>
+//                         </p>
+//                     </div>
+//                     </div>
+//                 </div>
 
-                </div>
-            </section> */}
+//                 </div>
             {/* End Resume Section */}
         </div>
     )
